e850775910bc129008f6987d3b77b434ca0b78e7
Joseph Boumerhi <joeyboumerhi@gmail.com>
2022-02-02 20:36:25 -0500

    Initial commit

---
+++ Assets/RotatorScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RotatorScript: MonoBehaviour
+{
+    public float rotationSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       /*
+        if (Input.GetKey(KeyCode.R))
+        {
+            this.GetComponent<Transform>().Rotate(0, rotationSpeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            this.GetComponent<Transform>().Rotate(0, -rotationSpeed, 0);
+        }
+     */
+    }
+
+    public void Rotate1(InputAction.CallbackContext context)
+    {
+            Debug.Log("R pressed");
+            this.GetComponent<Transform>().Rotate(0, rotationSpeed, 0);
+    }
+
+    public void Rotate2(InputAction.CallbackContext context)
+    {
+            Debug.Log("T pressed");
+            this.GetComponent<Transform>().Rotate(0, -rotationSpeed, 0);
+    }
+}
